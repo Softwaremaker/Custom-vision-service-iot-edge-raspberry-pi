5a0a423e1a7e31110d7744df0457b93cfe7af68b
Softwaremaker <1997252+Softwaremaker@users.noreply.github.com>
2018-08-26 13:13:51 +0800

    Update DisplayManager.py

--- modules/SenseHatDisplay/app/DisplayManager.py
+++ modules/SenseHatDisplay/app/DisplayManager.py
@@ -120,17 +120,19 @@ class DisplayManager(object):
         print("Displaying " + strImage)
         if 'apple' in strImage.lower():
             self.__displayImage(self.__apple())
-        elif 'rasp' in strImage.lower():
+        elif 'raspberry' in strImage.lower():
             self.__displayImage(self.__raspberry())
         elif 'banana' in strImage.lower():
             self.__displayImage(self.__banana())
         elif 'orange' in strImage.lower():
             self.__displayImage(self.__orange())
-        elif 'orange' in strImage.lower():
+        elif 'lemon' in strImage.lower():
             self.__displayImage(self.__lemon())
         elif 'none' in strImage.lower():
             self.s.clear()
+            #self.s.show_letter('?') #Prefer a test here to show SenseHat unit is working and listening
         else:
             self.__displayImage(self.__unknown())
             self.s.clear()
+            #self.s.show_letter('?') #Prefer a test here to show SenseHat unit is working and listening
 
